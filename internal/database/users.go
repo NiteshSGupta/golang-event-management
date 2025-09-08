73a7f2b1c74fafd7c2dadf14eb3b03fa29972655
nitesh.gupta.personal <ngdigital1122@gmail.com>
2025-09-08 19:02:53 +0530

    08/09/2025 evening changes

--- internal/database/users.go
+++ internal/database/users.go
@@ -27,14 +27,12 @@ func (m *UserModel) Insert(user *User) error {
 	return m.DB.QueryRowContext(ctx, query, user.Email, user.Password, user.Name).Scan(&user.Id)
 }
 
-func (m *UserModel) Get(id int) (*User, error) {
+func (m *UserModel) getUser(query string, args ...interface{}) (*User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	query := "SELECT * FROM users WHERE id = $1"
-
 	var user User
-	err := m.DB.QueryRowContext(ctx, query, id).Scan(&user.Id, &user.Email, &user.Name, &user.Password)
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.Id, &user.Email, &user.Name, &user.Password)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, nil
@@ -43,3 +41,29 @@ func (m *UserModel) Get(id int) (*User, error) {
 	}
 	return &user, nil
 }
+
+func (m *UserModel) Get(id int) (*User, error) {
+	query := "SELECT * FROM users WHERE id = $1"
+	return m.getUser(query, id)
+}
+func (m *UserModel) GetByEmail(email string) (*User, error) {
+	query := "SELECT * FROM users WHERE email = $1"
+	return m.getUser(query, email)
+}
+
+// func (m *UserModel) Get(id int) (*User, error) {
+// 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+// 	defer cancel()
+
+// 	query := "SELECT * FROM users WHERE id = $1"
+
+// 	var user User
+// 	err := m.DB.QueryRowContext(ctx, query, id).Scan(&user.Id, &user.Email, &user.Name, &user.Password)
+// 	if err != nil {
+// 		if err == sql.ErrNoRows {
+// 			return nil, nil
+// 		}
+// 		return nil, err
+// 	}
+// 	return &user, nil
+// }
